e9a4208824e671d96586a512cf484ca50c722903
Jessica Luo <luojess@student.ubc.ca>
2025-03-26 14:22:45 -0700

    modify script 01 to use new functions

--- scripts/01-download-dataset.R
+++ scripts/01-download-dataset.R
@@ -1,4 +1,4 @@
-### Script 1 without custom functions ###
+### Script 1 - without custom functions ###
 
 # url <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/refs/heads/master/data-raw/drug-use-by-age/drug-use-by-age.csv"
 # destination <- "data/raw/drug-use-by-age.csv"
@@ -9,7 +9,8 @@
 
 # download.file(url, destination)
 
-### Script 1 with custom functions ###
+### Script 1 - with custom functions ###
+
 source("R/create_directory.R")
 source("R/download_data.R")
 source("R/load_csv.R")

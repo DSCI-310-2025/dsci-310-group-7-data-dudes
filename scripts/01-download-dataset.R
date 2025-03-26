f171687adf2869981db0f6e8900d8750f804747f
Jessica Luo <luojess@student.ubc.ca>
2025-03-26 13:06:26 -0700

    modified script 01 to use new functions

--- scripts/01-download-dataset.R
+++ scripts/01-download-dataset.R
@@ -1,8 +1,19 @@
+### Script 1 without custom functions ###
+
+# url <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/refs/heads/master/data-raw/drug-use-by-age/drug-use-by-age.csv"
+# destination <- "data/raw/drug-use-by-age.csv"
+
+# if (!dir.exists(dirname(destination))) {
+#   dir.create(dirname(destination), recursive = TRUE)
+# }
+
+# download.file(url, destination)
+
+##$ Script 1 with custom functions ###
+source("R/data_utils.R")
+
 url <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/refs/heads/master/data-raw/drug-use-by-age/drug-use-by-age.csv"
 destination <- "data/raw/drug-use-by-age.csv"
 
-if (!dir.exists(dirname(destination))) {
-  dir.create(dirname(destination), recursive = TRUE)
-}
-
-download.file(url, destination)
+download_data(url, destination)
+df <- load_data(destination)

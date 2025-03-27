22e883ad3f6fabe08626e68679c5d8383dec39a3
Jade Chen <jadeeechen@gmail.com>
2025-03-26 17:12:38 -0700

    added the usage of create_directory() function

--- scripts/01-download-dataset.R
+++ scripts/01-download-dataset.R
@@ -18,5 +18,7 @@ source("R/load_csv.R")
 url <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/refs/heads/master/data-raw/drug-use-by-age/drug-use-by-age.csv"
 destination <- "data/raw/drug-use-by-age.csv"
 
+
+create_directory(destination)
 download_data(url, destination)
 data <- load_csv(destination)

59e9c5bbdcc559c873d1934a264da2b0d194974b
Jade Chen <jadeeechen@gmail.com>
2025-03-27 13:44:14 -0700

    add back deleted 01 functions and tests

---
+++ R/create_directory.R
@@ -0,0 +1,13 @@
+#' Create directory if it doesn't exist
+#'
+#' Checks if the specified directory exists, and
+#' creates the directory path if necessary.
+#'
+#' @param path A character string specifying the directory path.
+#'
+#' @return None. The function creates a directory as a side effect.
+create_directory <- function(path) {
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = TRUE)
+  }
+}
---
+++ R/load_csv.R
@@ -0,0 +1,9 @@
+#' Load CSV
+#'
+#' Reads a CSV file into a dataframe.
+#'
+#' @param file_path A character string specifying the path to the CSV file.
+#' @return A dataframe containing the data from the CSV file.
+load_csv <- function(file_path) {
+  readr::read_csv(file_path)
+}

f72ad61157915be187071877bf936ed274f2ae34
nazie23 <edroos.nazia@gmail.com>
2025-04-07 16:19:22 -0700

    sourced required R scripts in 01-download-dataset.R

--- scripts/01-download-dataset.R
+++ scripts/01-download-dataset.R
@@ -1,3 +1,7 @@
+source("R/create_directory.R")
+source("R/download_data.R")
+source("R/load_csv.R")
+
 url <- "https://raw.githubusercontent.com/rudeboybert/fivethirtyeight/refs/heads/master/data-raw/drug-use-by-age/drug-use-by-age.csv"
 destination <- "data/raw/drug-use-by-age.csv"
 
